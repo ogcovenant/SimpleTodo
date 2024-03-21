80d32641db257f70c4940070c42bf8a63925676f
Covenant Ogowale <justcovenant@outlook.com>
2024-03-21 17:31:37 +0100

    completed add to todo list functionality

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
+const todoList = document.querySelector(".todoList ul");
+const openAddTodoBtn = document.querySelector(".addTodo")
+const addTodoForm = document.querySelector(".addTodoForm")
+const cancelTodoBtn = document.querySelector(".cancelTodoBtn")
+const addTodoBtn = document.querySelector(".addTodoBtn");
+const addTodoInput = document.querySelector(".addTodoInput")
+
 const todo = [
   {
     name: "Wash Plates",
     status: "done"
-  }, {
-    name: "Wash clothes",
-    staus: "pending"
   }
 ]
 
-const todoList = document.querySelector(".todoList ul");
+const loadTodo = () => {
+  todo.map((todo, index) => {
+    const todoItem = document.createElement("li");
+    todoItem.classList.add("todoItem");
+    todoItem.classList.add(index);
+    todoItem.innerHTML = `<input type="checkbox" class="todoCheck">${todo.name}<div class="todoControls"><button class="editTodo"><i class="fa-solid fa-pen-to-square"></i></button> <button class="deleteTodo"><i class="fa-solid fa-trash"></i></button></div>`
+    todoList.appendChild(todoItem);
+  })
+}
 
-todo.map((todo, index) => {
-  const todoItem = document.createElement("li");
-  todoItem.classList.add("todoItem");
-  todoItem.classList.add(index);
-  todoItem.innerHTML = `<input type="checkbox" class="todoCheck">${todo.name}<div class="todoControls"><button class="editTodo"><i class="fa-solid fa-pen-to-square"></i></button> <button class="deleteTodo"><i class="fa-solid fa-trash"></i></button></div>`
-  todoList.appendChild(todoItem);
+const clearTodo = () => {
+  while(todoList.firstChild){
+    todoList.firstChild.remove()
+  }
+}
+
+loadTodo()
+
+openAddTodoBtn.addEventListener("click", () => {
+  addTodoForm.style.display = "flex"
 })
 
+cancelTodoBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  addTodoForm.style.display = "none"
+})
+
+addTodoBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  const todoItem = addTodoInput.value;
+  todo.push({
+    name: todoItem,
+    status: "pending"
+  })
+  addTodoInput.value = "";
+  addTodoForm.style.display = "none"
+  clearTodo()
+  loadTodo()
+})
