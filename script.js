578a2848e1cb5effed32504d84f8fea83dc524de
Covenant Ogowale <justcovenant@outlook.com>
2024-03-22 19:13:10 +0100

    done what i can do

--- script.js
+++ script.js
@@ -4,21 +4,25 @@ const addTodoForm = document.querySelector(".addTodoForm")
 const cancelTodoBtn = document.querySelector(".cancelTodoBtn")
 const addTodoBtn = document.querySelector(".addTodoBtn");
 const addTodoInput = document.querySelector(".addTodoInput")
+const editTodoForm = document.querySelector(".editTodoForm");
+const openEditTodoBtn = document.querySelector(".editTodo");
+const editTodoBtn = document.querySelector(".saveTodoBtn");
+const cancelTodoSaveBtn = document.querySelector(".cancelTodoSaveBtn");
+const editTodoInput = document.querySelector(".editTodoInput")
+const deleteTodoBtn = document.querySelector(".deleteTodo");
+let currentTodoValueIndex = 0;
 
-const todo = [
-  {
-    name: "Wash Plates",
-    status: "done"
-  }
-]
+
+const todo = []
 
 const loadTodo = () => {
-  todo.map((todo, index) => {
+  todo.forEach((todo, index) => {
     const todoItem = document.createElement("li");
     todoItem.classList.add("todoItem");
     todoItem.classList.add(index);
-    todoItem.innerHTML = `<input type="checkbox" class="todoCheck">${todo.name}<div class="todoControls"><button class="editTodo"><i class="fa-solid fa-pen-to-square"></i></button> <button class="deleteTodo"><i class="fa-solid fa-trash"></i></button></div>`
-    todoList.appendChild(todoItem);
+    todoItem.innerHTML = `<input type="checkbox" class="todoCheck"><span>${todo.name}</span><div class="todoControls"><button class="editTodo" onclick="openEditForm(event)" ><i class="fa-solid fa-pen-to-square"></i></button> <button class="deleteTodo" onclick="deleteTodo(event)" ><i class="fa-solid fa-trash"></i></button></div>`
+    todoList.append(todoItem);
+    
   })
 }
 
@@ -28,11 +32,43 @@ const clearTodo = () => {
   }
 }
 
-loadTodo()
+const openEditForm = async (e) => {
+  if(addTodoForm.style.display === "flex" || editTodoForm.style.display === "flex"){
+    addTodoForm.style.display = "none"
+    editTodoForm.style.display = "none"
+    editTodoForm.style.display = "flex"
+  }else{
+    editTodoForm.style.display = "flex"
+  }
 
-openAddTodoBtn.addEventListener("click", () => {
-  addTodoForm.style.display = "flex"
-})
+  currentTodoValueIndex = await e.target.parentElement.parentElement.classList[1]
+  console.log(todo[currentTodoValueIndex])
+  if(e.target.parentElement.parentElement.parentElement.children[1].innerHTML === undefined){
+    editTodoInput.value = e.target.parentElement.parentElement.children[1].innerHTML;
+  }else{
+    editTodoInput.value = e.target.parentElement.parentElement.parentElement.children[1].innerHTML;
+  }
+}
+
+
+const deleteTodo = (e) => {
+  const toDelete = e.target.parentElement.parentElement.classList[1];
+  todo.splice(toDelete, 1)
+  clearTodo()
+  loadTodo()
+}
+
+const edit = (e) => {
+  e.preventDefault()
+  const editedTodo = editTodoInput.value;
+  todo[currentTodoValueIndex].name = editedTodo
+  clearTodo()
+  loadTodo()
+  editTodoForm.style.display = "none"
+}
+
+clearTodo()
+loadTodo()
 
 cancelTodoBtn.addEventListener("click", (e) => {
   e.preventDefault()
@@ -50,4 +86,19 @@ addTodoBtn.addEventListener("click", (e) => {
   addTodoForm.style.display = "none"
   clearTodo()
   loadTodo()
+})
+
+cancelTodoSaveBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  editTodoForm.style.display = "none"
+})
+
+openAddTodoBtn.addEventListener("click", () => {
+  if(editTodoForm.style.display === "flex" || addTodoForm.style.display === "flex"){
+    editTodoForm.style.display = "none"
+    addTodoForm.style.display = "none"
+    addTodoForm.style.display = "flex"
+  }else{
+    addTodoForm.style.display = "flex"
+  }
 })
